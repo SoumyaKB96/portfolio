56d7bb6997cd78fc97712d561765ddaabe798ada
unknown <boity.soumyakanta@gmail.com>
2020-09-11 09:19:21 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import "./styles.css";
 import LandingPage from "./components/LandingPage";
 import SocialLinks from "./components/SocialLinks";
-import Contact from "./components/Contact";
+
 
 export default function App() {
   return (
     <div>
       <LandingPage />
       <SocialLinks />
-      <Contact />
+      
 
-      <img src="dpImage.jpg" alt="" className="Image" />
+      
     </div>
   );
 }
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <div className="foot_text">
-      <h1>Footer</h1>
-    </div>
-  );
-}
